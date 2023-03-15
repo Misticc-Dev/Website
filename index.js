f432561c748a6b6d2c59743d097e02b8f41605f8
Mistic <104326146+MisticPvP@users.noreply.github.com>
2023-03-15 17:42:10 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,127 @@
+//console.log("I like hotdogs");
+//console.log("It's really good");
+
+//window.alert("I REALLY LOVE HOTDOGS")
+
+//let firstName = "Noah" //strings
+//let age = 13; //number
+//let student = true; //boolean
+//let students = 20; //number
+//age = age + 1;
+
+//students += students + 1;
+//students += students - 1;
+//students += students * 2;
+//students += students / 2;
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+
+//let extraStudents = students % 3
+
+//console.log(extraStudents)
+//console.log(students)
+//console.log("Hello", firstName);
+//console.log("You are", age, "years old");
+//console.log("Enrolled: ", student);
+
+//document.getElementById("p1").innerHTML = "Hello " + firstName;
+//document.getElementById("p2").innerHTML = "You are " + age + " years old";
+//document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+//let username = window.prompt("What's your name");
+//console.log(username);
+
+//let age = window.prompt("How old are you?")
+
+//console.log(typeof age)
+//age = Number(age)
+//console.log(typeof age)
+//age += 1;
+
+//window.alert("Happy birthday!, you are now " + age +" years old")
+
+
+/*
+let age = 1;
+
+document.getElementById("myButton").onclick = function(){
+
+    age = document.getElementById("myText").value;
+    Sconsole.log(age);
+    document.getElementById("p1").innerHTML = "You are " + age + " years old";
+}
+*/
+/*
+let username;
+
+document.getElementById("myButton1").onclick = function(){
+
+    username = document.getElementById("myText1").value;
+    console.log(username);
+    document.getElementById("p2").innerHTML = "Your name is " + username;
+}
+*/
+
+//let x;
+//let y;
+//let z;
+
+//x = Number("3.14");
+//y = String(3.14);
+//z = Boolean("pizza");
+
+//console.log(x, typeof x);
+//console.log(y, typeof y);
+//console.log(z, typeof z;
+
+//const PI = 3.14159;
+//let radius;
+//let circumpherence;
+
+//PI = 420.69; // Not work because const
+
+//radius = window.prompt("Enter the radius of a circle");
+//radius = Number(radius);
+
+//circumpherence = 2 * PI * radius;
+
+//console.log("The circumpherence of the circle is: ", circumpherence);
+
+//let x = -3.14;
+//let y = 5;
+//let z = 9;
+//let maximum
+//let minimum
+
+//x = Math.round(x);
+//x = Math.floor(x);
+//x = Math.ceil(x);
+//x = Math.pow(x, 2);
+//x = Math.sqrt(x);
+//x = Math.abs(x);
+//maximum = Math.max(x, y, z);
+//minimum = Math.min(x, y, z);
+//x = Math.PI
+
+
+//console.log(x)
+
+
+let a;
+let b;
+let c;
+
+
+a = window.prompt("Enter side A: ");
+a = Number(a);
+
+b = window.prompt("Enter side B: ");
+b = Number(b);
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+console.log("Side C:", c);
+
